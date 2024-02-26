834152df0e76608ec889993fab1225163aef3b6c
andrewdevelo <lemeshev.andrei@gmail.com>
2024-02-26 18:23:16 +0300

    feat: add method get_all_courses       add template all_courses.html

--- apps/online_courses/views.py
+++ apps/online_courses/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from apps.course.models import Course
 
 
 def home_page(request):
     return render(
         request=request,
         template_name='main.html'
+    )
+
+
+def get_all_courses(request):
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses
+    }
+
+    return render(
+        request=request,
+        template_name='online_courses/all_courses.html',
+        context=context
     )
